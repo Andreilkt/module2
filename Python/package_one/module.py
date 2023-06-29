cf73c0e3e5fce1e966eaf2dbd7580f92f831dec2
andrei <andreilkt@yandex.ru>
2023-06-29 23:08:10 +0700

    Изменены модули

--- Python/package_one/module.py
+++ Python/package_one/module.py
@@ -1,14 +1,13 @@
 # Импортируемый модуль и класс  для импорта User
 
+
 class User:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-  def greet(self):
-    print(f'Привет, я {self.name}')
+    def greet(self):
+        print(f'Привет, я {self.name}')
 
-  def birth_year(self):
-    print(f'Дата рождения {2021 - self.age}')
-
-
+    def birth_year(self):
+        print(f'Дата рождения {2021 - self.age}')
--- Python/package_two/module1.py
+++ Python/package_two/module1.py
@@ -1,12 +1,10 @@
 # Импорт класса User из модуля module
 
-from ..package_one.module import User
-#from Python.package_one.module import User
-
+#from ..package_one.module import User
+from Python.package_one.module import User
 
 
 class user_info(User):
-
     pass
 
 # Новый объект Person из класса Person)
@@ -15,5 +13,3 @@ person1 = User("Andrey", 20)
 # Вызвать методы
 person1.greet()
 person1.birth_year()
-
-
